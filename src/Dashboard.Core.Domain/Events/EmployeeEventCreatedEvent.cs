8f7135b9bb08821b916f789fcfc0fcc446fa4bba
mwilayat <m.wilayat.hussain@gmail.com>
2025-06-12 01:54:27 +0300

    remvoed

--- src/Dashboard.Core.Domain/Events/EmployeeEventCreatedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dashboard.Core.Domain.Entities;
-
-namespace Dashboard.Core.Domain.Events;
-public record EmployeeEventCreatedEvent(EmployeeEvent EmployeeEvent);
